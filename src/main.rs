b429e396848f89e0d7af233905fde4f563ff09a9
joeymanny <joeymanny1212@gmail.com>
2024-01-05 16:41:35 -0900

    working mandelbrot

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use wgpu::{PipelineLayoutDescriptor, RenderPipelineDescriptor};
 
+
+use wgpu::{PipelineLayoutDescriptor, RenderPipelineDescriptor, util::DeviceExt};
+use winit::{event::{MouseButton, KeyEvent}, keyboard::{KeyCode, Key, PhysicalKey}};
 fn main(){
     let event_loop = winit::event_loop::EventLoop::new().unwrap();
     let primary_size = event_loop.primary_monitor().unwrap().size();
     // sure hope nobody has a monitor less than 10 pixels 
-    let width = (primary_size.width / 6).max(1);
-    let height = (primary_size.height / 6).max(1);
+    let size = std::cmp::min(primary_size.width, primary_size.height);
     let window = winit::window::WindowBuilder::new()
-        .with_min_inner_size(winit::dpi::PhysicalSize{width,height,})
-        .with_title("click within inner to toggle window decorations")
+        // .with_min_inner_size(winit::dpi::PhysicalSize{width,height,})
+        .with_inner_size(winit::dpi::PhysicalSize::new(size,size))
+        .with_title("mandelbrot set")
         .build(&event_loop)
         .unwrap();
     env_logger::init();
@@ -35,13 +37,56 @@ async fn run(event_loop: winit::event_loop::EventLoop<()>, window: winit::window
         },
         None
     ).await.expect("couldn't find an adequate device");
+    let mandel_commands: &mut [f32; 6] = &mut [
+        size.width as f32, size.height as f32, // screen dimensions
+        0.0, 0.0, // linear offset
+        2.0, // zoom
+        300.0, // level of detail (num mandel iterations)
+    ];
+    let staging_mandel_commands_buffer = device.create_buffer(&wgpu::BufferDescriptor{
+        label: None,
+        size: std::mem::size_of::<f32>() as u64 * 6,
+        usage: wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::MAP_WRITE,
+        mapped_at_creation: false,
+    });
+    let mandel_commands_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor{
+        label: None,
+        contents: bytemuck::cast_slice(mandel_commands),
+        usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST
+
+    });
+    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor{
+        label: None,
+        entries: &[wgpu::BindGroupLayoutEntry{
+            binding: 0,
+            visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
+            ty: wgpu::BindingType::Buffer{
+                ty: wgpu::BufferBindingType::Uniform,
+                has_dynamic_offset: false,
+                min_binding_size: None,
+            },
+            count: None,
+        }]
+    });
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor{
+        label: None,
+        layout: &bind_group_layout,
+        entries: &[wgpu::BindGroupEntry{
+            binding: 0,
+            resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding{
+                buffer: &mandel_commands_buffer,
+                offset: 0,
+                size: None,
+            })
+        }]
+    });
     let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor{
         label: None,
         source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(include_str!("shader.wgsl")))
     });
     let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor{
         label: None,
-        bind_group_layouts: &[],
+        bind_group_layouts: &[&bind_group_layout],
         push_constant_ranges: &[],
     });
     let swapchain_capabilities = surface.get_capabilities(&adapter);
@@ -79,16 +124,23 @@ async fn run(event_loop: winit::event_loop::EventLoop<()>, window: winit::window
     move |event, target|{
         // so they get cleaned up since run() never returns
         let _ = (&instance, &adapter, &shader, &pipeline_layout);
-
+        let mut is_mandel_update = false;
         if let winit::event::Event::WindowEvent {
             event, // shadowing
             window_id: _
         } = event {
         match event {
             winit::event::WindowEvent::Resized(new_size) => {
+                println!("resize");
+
                 config.width = new_size.width.max(1);
                 config.height = new_size.height.max(1);
                 surface.configure(&device, &config);
+
+                mandel_commands[0] = new_size.width as f32;
+                mandel_commands[1] = new_size.height as f32;
+                is_mandel_update = true;
+
                 window.request_redraw();
             }
             winit::event::WindowEvent::RedrawRequested =>{
@@ -108,25 +160,91 @@ async fn run(event_loop: winit::event_loop::EventLoop<()>, window: winit::window
                                 view: &view,
                                 resolve_target: None,
                                 ops: wgpu::Operations {
-                                    load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+                                    load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
                                     store: wgpu::StoreOp::Store,
                                 }
                             })],
                             ..Default::default()
                         });
+                        rpass.set_bind_group(0, &bind_group, &[]);
                         rpass.set_pipeline(&render_pipeline);
-                        rpass.draw(0..3, 0..1);
+                        rpass.draw(0..3, 0..2);
                 }
                 queue.submit(Some(encoder.finish()));
                 frame.present();
             },
             winit::event::WindowEvent::CloseRequested => target.exit(),
-            winit::event::WindowEvent::MouseInput {state, ..} => {
-                if state.is_pressed(){
-                    window.set_decorations(!window.is_decorated());
+            winit::event::WindowEvent::MouseWheel {delta, ..} => {
+                    if let winit::event::MouseScrollDelta::LineDelta(_, y) = delta{
+                        if y.is_sign_positive(){
+                            is_mandel_update = true;
+                            mandel_commands[4] -= 1.0;
+                            window.request_redraw();
+                        }else if y.is_sign_negative(){
+                            is_mandel_update = true;
+                            mandel_commands[4] = (mandel_commands[4] + 1.0).min(2.0);
+                            window.request_redraw();
+                        }
+                    }
+            },
+            winit::event::WindowEvent::KeyboardInput {event, ..}=>{
+
+                if let KeyEvent{physical_key: winit::keyboard::PhysicalKey::Code(key), state: winit::event::ElementState::Pressed, ..} = event{
+                    let mut delta_offset: (i8, i8) = (0, 0);
+                    if let KeyCode::ArrowLeft | KeyCode::KeyA = key{
+                        delta_offset.0 += -1;
+                    }
+                    if let KeyCode::ArrowRight | KeyCode::KeyD = key{
+                        delta_offset.0 += 1;
+                    }
+                    if let KeyCode::ArrowUp | KeyCode::KeyW = key{
+                        delta_offset.1 += -1; // 0,0 is at top-left
+                    }
+                    if let KeyCode::ArrowDown | KeyCode::KeyS = key{
+                        delta_offset.1 += 1;
+                    }
+                    if delta_offset != (0, 0){
+                        is_mandel_update = true;
+                        mandel_commands[2] += (2.0f32.powf(mandel_commands[4]) / 4.0) * delta_offset.0 as f32;
+                        mandel_commands[3] += (2.0f32.powf(mandel_commands[4]) / 4.0) * delta_offset.1 as f32;
+                        window.request_redraw();
+                    }
+                    let mut delta_lod: i8 = 0;
+                    if let KeyCode::Equal = key{
+                        delta_lod += 1;
+                    }
+                    if let KeyCode::Minus = key{
+                        delta_lod += -1;
+                    }
+                    if delta_lod != 0 {
+                        mandel_commands[5] *= 2.0_f32.powi(delta_lod.into());
+                        is_mandel_update = true;
+                        window.request_redraw();
+                    }
                 }
             },
             _ => (),
+            }
+        }
+        if is_mandel_update{
+            dbg!(&mandel_commands);
+            let (sender, receiver) = flume::bounded(1);
+            let slice = staging_mandel_commands_buffer.slice(..);
+            slice.map_async(wgpu::MapMode::Write, move |v| sender.send(v).unwrap());
+            device.poll(wgpu::Maintain::Wait);
+
+            if let Ok(Ok(())) = receiver.recv(){
+                let mut mapped = slice.get_mapped_range_mut();
+                mapped.clone_from_slice(bytemuck::cast_slice(mandel_commands));
+                drop(mapped);
+                staging_mandel_commands_buffer.unmap();
+                let mut encoder = device.create_command_encoder(&Default::default());
+                encoder.copy_buffer_to_buffer(
+                    &staging_mandel_commands_buffer, 0,
+                    &mandel_commands_buffer, 0,
+                    std::mem::size_of::<f32>() as u64 * 6,
+                );
+                queue.submit(Some(encoder.finish()));
             }
         }
     })
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,15 +1,72 @@
-@vertex
-fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
-    let x = f32(i32(in_vertex_index) - 1);
-    let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
-    return vec4<f32>(x, y, 0.0, 1.0);
+@group(0) @binding(0) var<uniform> cmd: MandelCommands;
+
+struct MandelCommands{
+    size: vec2<f32>,
+    offset: vec2<f32>,
+    zoom: f32,
+    lod: f32
 }
 
-@fragment
-fn fs_main(@builtin(position) coord_in: vec4<f32>) -> @location(0) vec4<f32> {
+@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32, @builtin(instance_index) instance_index: u32) -> @builtin(position) vec4<f32> {
+    // let x = f32(i32(in_vertex_index) - 1);
+    // let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
+    var x: f32 = 0.0;
+    var y: f32 = 0.0;
+    // square
+    if in_vertex_index == u32(0){
+        x = -1.;
+        y = -1.;
+    } else if in_vertex_index == u32(1){
+        if instance_index == u32(0){
+            x = -1.;
+            y = 1.;
+        }else{
+            x = 1.;
+            y = -1.;
+        }
+    }else if in_vertex_index == u32(2){
+        x = 1.;
+        y = 1.;
+    }
 
-    return vec4<f32>(1.0, 0.5, 0.0, 1.0);
-    // return coord_in * f32(0.5);
+    // triangle
+    // if in_vertex_index == u32(0){
+    //     x = -1.;
+    //     y = -1.;
+    // }else if in_vertex_index == u32(1){
+    //     x = 0.;
+    //     y = 1.
+    // }else{
+    //     x = 1.;
+    //     y = -1.;
+    // }
+    return vec4<f32>(f32(x), f32(y), 0.0, 1.0);
+}
+
+
+@fragment
+fn fs_main(@builtin(position) pos: vec4<f32>) -> @location(0) vec4<f32> {
+    // let c1 = vec3(0.988, 0.196, 0.31);
+    // let c2 = vec3(0.137, 0.659, 0.949);
+    // let c1 = vec3(1., 0., 0.,);
+    // let c2 = vec3(0., 0., 1.);
+    
+    var newpos = pos.xy / cmd.size;
+    newpos.x = ((newpos.x - 0.5) * (cmd.size.x / cmd.size.y)) * pow(2.0, cmd.zoom);
+    newpos.y = (newpos.y - 0.5) * pow(2.0, cmd.zoom);
+    newpos += cmd.offset;
+
+    let mand = f32(mandel(Complex(newpos.x, newpos.y))) / cmd.lod;
+    // var circle = vec3(.5, .5, .3);
+
+    // var dist = length(newpos - circle.xy) - circle.z;
+
+    // dist = smoothstep(0.0, 0.005, dist);
+
+    let col = mix(vec3(0.0, 0.0, 0.0), vec3(1.0, 1.0, 1.0), mand - .1);
+    
+    return vec4<f32>(col, 1.0);
 
 }
 
@@ -27,7 +84,7 @@ fn mandel_iter(in: Complex, c: Complex) -> Complex {
 }
 
 fn sqr(in: Complex) -> Complex {
-    return Complex(f32(pow(in.real, 2.0) - pow(in.imag, 2.0)), f32(f32(2.0) * in.real * in.imag));
+    return Complex(pow(in.real, 2.0) - pow(in.imag, 2.0), f32(2.0) * in.real * in.imag);
 }
 
 fn abs_sq(in: Complex) -> f32 {
@@ -37,11 +94,11 @@ fn abs_sq(in: Complex) -> f32 {
 fn mandel(c: Complex) -> u32 {
     var z = Complex(0.0, 0.0);
     var i: u32 = u32(0);
-    while (i < u32(2000) ){
+    while (i < u32(cmd.lod)){
         i += u32(1);
         z = mandel_iter(z, c);
         if abs_sq(z) > 4.0 {
-            break;
+            return i;
         }
     }
     return i;
